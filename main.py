4e7a1fcd2a7ff00d8fee30e244e6b7dd2f2b9f39
ChoongGao Lee <choonggao.lee@utexas.edu>
2019-12-24 15:20:15 -0600

    Tried to change to use sound objects

--- main.py
+++ main.py
@@ -10,23 +10,30 @@ def register_sound(key, sound):
     key_sounds[key] = sound
 
 def register_sounds():
-    register_sound(pygame.K_a, pygame.mixer.Sound("A4.wav"))
-    register_sound(pygame.K_s, pygame.mixer.Sound("B4.wav"))
-    register_sound(pygame.K_d, pygame.mixer.Sound("C5.wav"))
-    register_sound(pygame.K_f, pygame.mixer.Sound("D5.wav"))
-    register_sound(pygame.K_g, pygame.mixer.Sound("E5.wav"))
-    register_sound(pygame.K_h, pygame.mixer.Sound("F5.wav"))
-    register_sound(pygame.K_j, pygame.mixer.Sound("G5.wav"))
-    register_sound(pygame.K_k, pygame.mixer.Sound("A5.wav"))
+    # register_sound(pygame.K_a, pygame.mixer.Sound("A4.wav"))
+    # register_sound(pygame.K_s, pygame.mixer.Sound("B4.wav"))
+    # register_sound(pygame.K_d, pygame.mixer.Sound("C5.wav"))
+    # register_sound(pygame.K_f, pygame.mixer.Sound("D5.wav"))
+    # register_sound(pygame.K_g, pygame.mixer.Sound("E5.wav"))
+    # register_sound(pygame.K_h, pygame.mixer.Sound("F5.wav"))
+    # register_sound(pygame.K_j, pygame.mixer.Sound("G5.wav"))
+    # register_sound(pygame.K_k, pygame.mixer.Sound("A5.wav"))
+    rate, sound = wavfile.read('A4.wav')
+    sounds = [pygame.sndarray.make_sound(sound)]
+    dict_sound[pygame.K_a] = sounds[0]
+    is_playing[pygame.K_a] = False
+    return dict_sound, is_playing
+
 
 def init():
         rate, sound = wavfile.read('A4.wav')
         pygame.mixer.init(rate, -16, 1, 2048)
-        register_sounds()
+        dict_sound, is_playing = register_sounds()
         pygame.init()
         os.environ['SDL_VIDEO_CENTERED'] = '1'
         pygame.display.set_caption('Keyboard')
         screen = pygame.display.set_mode((800, 600))
+        return dict_sound, is_playing
 
 
 
@@ -36,18 +43,23 @@ def playSound(key_obj):
 
 
 def startPiano():
-    init()
+    dict_sound, is_playing = init()
     shouldQuit = False
     while not shouldQuit:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 print(event.key)
-                if event.key in key_sounds:
-                    playSound(event.key)
+                if event.key in dict_sound and not is_playing[event.key]:#key_sounds:
+                    dict_sound[event.key].play(fade_ms=50)
+                    is_playing[event.key] = True
+                    # playSound(event.key)
                 if event.key == pygame.K_q:
                     shouldQuit = True
             elif event.type == pygame.KEYUP:
-                pass
+                if is_playing[event.key]:
+                    dict_sound[event.key].fadeout(50)
+                is_playing[event.key] = False
+                # pass
             if event.type == pygame.QUIT:
                 shouldQuit = True
 
